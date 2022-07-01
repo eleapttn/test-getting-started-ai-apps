e243ed80204542a373cf09a8186f318a1608afa3
Eléa Petton <78957502+eleapttn@users.noreply.github.com>
2022-07-01 15:38:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def HelloWorld():
 
-    #data = request.get_json()
-    #response = 'Hello ' + data + '. Congratulations, you have launched your first AI App!'
-    response = {
-        'response': 'Hello world!'
-    }
+    data = request.get_json()
+    response = 'Hello ' + data + '. Congratulations, you have launched your first AI App!'
+
     return jsonify(response)
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
